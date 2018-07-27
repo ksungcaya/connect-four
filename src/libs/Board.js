ef76356b1a9f8ca7a7f3ddc1162a6b62e5c0b1c0
Kenneth Sungcaya <ksungcaya@gmail.com>
2018-07-27 15:51:32 +0800

    initial board class

---
+++ src/libs/Board.js
@@ -0,0 +1,98 @@
+import _ from 'lodash';
+
+class Board {
+  /**
+   * Board constructor.
+   *
+   * @param {Integer} columns
+   * @param {Integer} rows
+   */
+  constructor(columns = 7, rows = 6) {
+    this.columns = columns;
+    this.rows = rows;
+
+    this.cells = {};
+    this.cellsCount = this.columns * this.rows;
+  }
+
+  /**
+   * Player move on the column.
+   *
+   * @param {Player} player
+   * @param {Integer} column
+   */
+  drop(player, column) {
+    const row = this._getFreeRow(column);
+
+    if (row === -1) {
+      throw new Error(`All rows of column: ${column} were occupied.`);
+    }
+
+    const key = this._cellKey(column, row);
+
+    this.cells[key] = player;
+    this.cellsCount -= 1;
+  }
+
+  /**
+   * Gets the current player occupying the cell.
+   *
+   * @param {Integer} column
+   * @param {Integer} row
+   *
+   * @return {Object<Player>}
+   */
+  cellPlayer(column, row) {
+    const key = this._cellKey(column, row);
+
+    if (_.has(this.cells, key)) {
+      return this.cells[key];
+    }
+
+    return null;
+  }
+
+  /**
+   * Get the number of unoccupied cells.
+   *
+   * @returns {Integer}
+   */
+  availableCells() {
+    return this.cellsCount;
+  }
+
+  /**
+   * Get the free row from the column.
+   * If all rows were occupied, return -1.
+   *
+   * @param {Integer} column
+   *
+   * @returns {Integer}
+   */
+  _getFreeRow(column) {
+    for (let row = 1; row <= this.rows; row += 1) {
+      const key = this._cellKey(column, row);
+
+      if (!_.has(this.cells, key)) {
+        return row;
+      }
+    }
+
+    return -1;
+  }
+
+  /**
+   * Create a key combination of column and row
+   * that we can use when accessing cells.
+   *
+   * @param {Integer} column
+   * @param {Integer} row
+   *
+   * @returns {String}
+   */
+  _cellKey(column, row) {
+    return `${column}_${row}`;
+  }
+}
+
+export default Board;
---
+++ tests/libs/Board.test.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+/* eslint no-unused-expressions: 0 */
+import { expect } from 'chai';
+import Board from '../../src/libs/Board';
+import Player from '../../src/libs/Player';
+
+describe('BoardTest', () => {
+  const board = new Board();
+  const player1 = new Player('red');
+  const player2 = new Player('yellow');
+
+  it('initializes default cells', () => {
+    expect(board.columns).to.equals(7);
+    expect(board.rows).to.equals(6);
+  });
+
+  describe('#drop', () => {
+    it('assigns the player to the cell it dropped', () => {
+      board.drop(player1, 2);
+      board.drop(player2, 2);
+
+      expect(player1).to.be.deep.equal(board.cellPlayer(2, 1));
+      expect(player2).to.be.deep.equal(board.cellPlayer(2, 2));
+    });
+
+    it('throws an error if all rows of the column were occupied', () => {
+      board.drop(player1, 3);
+      board.drop(player2, 3);
+      board.drop(player1, 3);
+      board.drop(player2, 3);
+      board.drop(player1, 3);
+      board.drop(player2, 3);
+
+      // this will throw an error
+      expect(board.drop.bind(board, player1, 3)).to.throw();
+    });
+
+    it('gets count of unoccupied cells', () => {
+      const smallBoard = new Board(1, 1);
+      smallBoard.drop(player1, 1);
+
+      expect(smallBoard.availableCells()).to.be.equals(0);
+    });
+  });
+});
